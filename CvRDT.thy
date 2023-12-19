1d8bcd52cfc141b562ad712527206d66a92d4795
Alexandre Piveteau <alexandre@piveteau.email>
2023-12-19 14:41:31 +0100

    Add PNSet.remove

--- CvRDT.thy
+++ CvRDT.thy
@@ -102,7 +102,7 @@ end
 export_code
   "merge"
   "USet" "uset_add" "uset_value"
-  "PNSet" "pnset_add" "pnset_value"
+  "PNSet" "pnset_add" "pnset_remove" "pnset_value"
 in Scala 
   module_name "CvRDT"
 
