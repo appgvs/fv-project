08a9131fdba753e7a142aff1817458b49c165e1e
Victor Schneuwly <victor.schneuwly@epfl.ch>
2023-12-19 16:51:31 +0100

    Sledgehammer for USet

--- USet.thy
+++ USet.thy
@@ -36,7 +36,28 @@ interpretation uset_cvrdt : CvRDT
     Fun.id
     USet.add
 proof
-    (*show "\<And>a u. a \<subseteq> add_to_set a u" by auto*)
+  show "\<And>x. USet.subset_eq x x"
+    using subset_eq.elims(3) by fastforce
+  show "\<And>x y. USet.subset x y = (USet.subset_eq x y \<and> \<not> USet.subset_eq y x)"
+    by (metis elements.elims psubsetE psubsetI subset.simps subset_eq.simps)
+  show "\<And>x y z.
+       USet.subset_eq x y \<Longrightarrow>
+       USet.subset_eq y z \<Longrightarrow> USet.subset_eq x z"
+    by (metis USet.inject subset_eq subset_eq.elims(2) subset_eq.elims(3))
+  show "\<And>x y. USet.subset_eq x y \<Longrightarrow>
+           USet.subset_eq y x \<Longrightarrow> x = y"
+    using subset_eq.elims(2) by fastforce
+  show "\<And>x y. USet.subset_eq x (USet.union x y)"
+    by (metis USet.exhaust Un_commute inf_sup_ord(4) subset_eq.simps union.simps)
+  show  "\<And>y x. USet.subset_eq y (USet.union x y)"
+    by (metis elements.simps inf_sup_ord(4) subset_eq.elims(3) union.elims)
+  show "\<And>y x z.
+       USet.subset_eq y x \<Longrightarrow>
+       USet.subset_eq z x \<Longrightarrow>
+       USet.subset_eq (USet.union y z) x"
+    by (smt (verit, ccfv_threshold) elements.simps le_sup_iff subset_eq.elims(2) subset_eq.elims(3) union.elims)
+  show "\<And>a u. USet.subset_eq a (add a u)"
+    using subset_eq.elims(3) by fastforce
 qed
 
 end
