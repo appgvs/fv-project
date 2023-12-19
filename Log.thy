5c949ab34416e8ed19c2b6f8d724cb612282f100
pgilliar <patrick.gilliard@epfl.ch>
2023-12-19 20:37:56 +0100

    fix Log

--- Log.thy
+++ Log.thy
@@ -6,6 +6,8 @@ type_synonym Timestamp = "nat list"
 
 datatype 'event LogEntry = LogEntry "'event" "Timestamp"
 datatype 'event Log = Log "('event LogEntry) set"
+datatype 'event NewEvent = NewEvent "'event" "nat" "Timestamp"
+
 
 definition initial :: "'event Log" where
   "initial = Log Set.empty"
@@ -15,11 +17,11 @@ fun vector_clock_update :: "nat => Timestamp => Timestamp" where
   "vector_clock_update i v = (let incremented = (nth v i) + 1 in
                                 take i v @ [incremented] @ drop (i+1) v)"
 
-fun update :: "'event Log => 'event => nat => Timestamp => 'event Log" where
-  "update (Log s) e i v = Log (s \<union> {LogEntry e (vector_clock_update i v)})"
+fun update :: "'event Log \<Rightarrow>'event  NewEvent => 'event Log" where
+  "update (Log s) (NewEvent e i v) = Log (s \<union> {LogEntry e (vector_clock_update i v)})"
 
-fun merge :: "'event Log => 'event Log => 'event Log" where
-  "merge (Log s1) (Log s2) = Log (s1 \<union> s2)"
+fun elements :: "'event Log => ('event LogEntry) set" where
+  "elements (Log s) = s"
 
 fun gc :: "'event Log => Timestamp => 'event Log" where
   "gc (Log s) min_ts = Log {entry. entry \<in> s \<and> (\<exists>e t. entry = LogEntry e t \<and> (\<forall>j<length t. nth t j <= nth min_ts j))}"
@@ -40,13 +42,13 @@ interpretation LogCvRDT : CvRDT
   Log.subset
   Log.union
   Log.initial
+  Log.elements
   Log.update
-  Log.merge
 proof
   show "\<And>x. Log.subset_eq x x"
     using subset_eq.elims(3) by fastforce
   show "\<And>x y. Log.subset x y = (Log.subset_eq x y \<and> \<not> Log.subset_eq y x)"
-    by (metis subset.elims psubsetE psubsetI subset.simps subset_eq.simps)
+    by (metis elements.elims psubsetE psubsetI subset.simps subset_eq.simps)
   show "\<And>x y z.
        Log.subset_eq x y \<Longrightarrow>
        Log.subset_eq y z \<Longrightarrow> Log.subset_eq x z"
@@ -62,9 +64,9 @@ proof
        Log.subset_eq y x \<Longrightarrow>
        Log.subset_eq z x \<Longrightarrow>
        Log.subset_eq (Log.union y z) x"
-    by (metis Log.inject subset_eq subset_eq.elims(2) subset_eq.elims(3))
-  show "\<And>a u. Log.subset_eq a (merge a u)"
+    by (smt (verit, ccfv_threshold) elements.simps le_sup_iff subset_eq.elims(2) subset_eq.elims(3) union.elims)  
+  show "\<And>a u. Log.subset_eq a (update a u)"
     using subset_eq.elims(3) by fastforce
-  qed
+qed
   
 end
