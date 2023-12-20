5714b0d92cd74603d0f337879d98ec6209d47ad7
Alexandre Piveteau <alexandre@piveteau.email>
2023-12-20 15:49:56 +0100

    Add `ROOT` and remove `IntegerVector`

--- IntegerVector.thy
+++ IntegerVector.thy
@@ -1,5 +1,5 @@
 theory IntegerVector
-    imports CvRDT HOL.Map HOL.List
+    imports CvRDT HOL.List
 begin
 
 type_synonym IntegerVector = "nat list"
@@ -7,80 +7,283 @@ type_synonym Update = "nat"
 
 definition "initial = []"
 
-fun current :: "IntegerVector => nat List.list" where
-  "current xs = xs"
+fun query :: "IntegerVector => nat list" where
+    "query xs = xs"
 
 fun update :: "IntegerVector => Update => IntegerVector" where
-  "update [] 0 = [1]" |
-  "update [] n = 0#(update [] (n-1))" |
-  "update (x#xs) 0 = (x+1)#xs" |
-  "update (x#xs) n = x#(update xs (n-1))"
+    "update []      0 = [1]" |
+    "update (x#xs)  0 = ((x+1)#xs)" |
+    "update []      n = (0#(update [] (n-1)))" |
+    "update (x#xs)  n = (x#(update xs (n-1)))"
 
-fun less_eq :: "IntegerVector \<Rightarrow> IntegerVector \<Rightarrow> bool" where
-  "less_eq [] _ = True" |
-  "less_eq (0#xs) [] = less_eq xs []" |
-  "less_eq (x#xs) [] = False" |
-  "less_eq (x#xs) (y#ys) = ((x \<le> y) & less_eq xs ys)"
+fun less_eq :: "IntegerVector => IntegerVector => bool" where
+    "less_eq [] _ = True" |
+    "less_eq (x#xs) [] = False" |
+    "less_eq (x#xs) (y#ys) = ((x \<le> y) & less_eq xs ys)"
 
-lemma less_eq_self : "less_eq x x"
+lemma less_eq_reflexive : "less_eq x x"
   apply (induct x)
   apply (auto)
   done
 
-lemma update_monotonic_induct : "less_eq a2 (update a2 u) \<Longrightarrow> less_eq (a1 # a2) (update (a1 # a2) u)"
-  sorry
+lemma less_eq_antisymmetry: "less_eq x y \<Longrightarrow> less_eq y x \<Longrightarrow> x = y"
+proof (induct x arbitrary: y)
+  case Nil
+  then show ?case
+    using less_eq.elims(2) by auto
+next
+  case (Cons x xs)
+  then show ?case
+  proof (induct y)
+    case Nil
+    then show ?case
+      apply(auto)
+      done
+  next
+    case (Cons y ys)
+    then show ?case
+      apply(auto)
+      done
+  qed
+qed
 
-lemma update_monotonic : "less_eq a (update a u)"
-  apply (induct a)
+lemma less_eq_transitive: "less_eq x y \<Longrightarrow> less_eq y z \<Longrightarrow> less_eq x z"
+proof (induct x arbitrary: y z)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons x xs)
+  then show ?case
+  proof (induct y arbitrary: z)
+    case Nil
+    then show ?case by simp
+  next
+    case (Cons y ys)
+    then show ?case
+    proof (induct z)
+      case Nil
+      then show ?case by simp
+    next
+      case (Cons z zs)
+      then show ?case
+        by (meson dual_order.trans less_eq.simps(3))
+    qed
+  qed
+qed
+
+fun less :: "IntegerVector => IntegerVector => bool" where
+    "less _ [] = False" |
+    "less [] (y#ys) = True" |
+    "less (x#xs) (y#ys) = (((x < y) & less_eq xs ys) | ((x = y) & less xs ys))"
+
+lemma less_comb : "less x y = (less_eq x y & (~less_eq y x))"
+proof (induct x arbitrary: y)
+  case Nil
+  then show ?case
+  proof (induct y)
+    case Nil
+    then show ?case
+      apply (auto)
+      done
+  next
+    case (Cons y ys)
+    then show ?case
+      apply (auto)
+      done
+  qed
+next
+  case (Cons x xs)
+  then show ?case
+  proof (induct y)
+    case Nil
+    then show ?case
+      apply (auto)
+      done
+  next
+    case (Cons y ys)
+    then show ?case
+      using less_eq.simps(3) less_eq_antisymmetry less_eq_reflexive nat_less_le by force
+  qed
+qed
+
+fun merge :: "IntegerVector => IntegerVector => IntegerVector" where
+    "merge v1 [] = v1" |
+    "merge [] v2 = v2" |
+    "merge (x#xs) (y#ys) = ((if (x > y) then x else y) #(merge xs ys))"
+
+lemma less_eq_merge_left: "less_eq x (merge x y)"
+proof (induct y arbitrary: x)
+  case Nil
+  then show ?case
+    apply (auto)
+    by (simp add: less_eq_reflexive)
+next
+  case (Cons a y)
+  then show ?case
+  proof (induct x)
+    case Nil
+    then show ?case
+      apply (auto)
+      done
+  next
+    case (Cons x xs)
+    then show ?case
+      apply (auto)
+      done
+  qed
+qed
+
+lemma merge_empty_left : "merge [] y = y"
+  apply (induct y)
   apply (auto)
-  by (simp add: update_monotonic_induct)
+  done
 
+(*lemma merge_non_empty_left: "a \<noteq> [] \<Longrightarrow> merge a b \<noteq> []"
+proof (rule ccontr)
+  assume "a \<noteq> []" and "merge a b = []"
+  then obtain x xs where "a = x # xs" by (cases a, auto)
+  then have "merge (x # xs) b \<noteq> []" by (cases b, simp_all)
+  with `merge a b = []` show False by simp
+qed*)
 
-fun less :: "IntegerVector \<Rightarrow> IntegerVector \<Rightarrow> bool" where
-  "less _ []  = False" |
-  "less [] (0#ys) = less [] ys" |
-  "less [] (y#ys) = True" | 
-  "less (x#xs) (y#ys) = (((x = y) & less xs ys) | ((x < y) & less_eq xs ys))"
+lemma merge_empty_right : "merge x [] = x"
+  apply (induct x)
+  apply (auto)
+  done
 
-lemma less_comb  : "less x y = (less_eq x y & (~less_eq y x))"
-  apply (induction x arbitrary: y)
-  by auto
+lemma merge_commutativity : "merge x y  = merge y x"
+proof (induct x arbitrary: y)
+  case Nil
+  then show ?case
+    apply (auto)
+    apply (simp add: merge_empty_left)
+    done
+next
+  case (Cons a as)
+  then show ?case
+  proof (induct y)
+    case Nil
+    then show ?case
+      apply (auto)
+      done
+    case (Cons b bs)
+    then show ?case
+      apply (auto)
+      done
+  qed
+qed
 
-fun max :: "IntegerVector \<Rightarrow> IntegerVector \<Rightarrow> IntegerVector" where
-  "max [] xs = xs" |
-  "max xs [] = xs" |
-  "max (x#xs) (y#ys) = (if x > y then x else y)#max xs ys"
-  
-interpretation IntVectorCvRDT : CvRDT
+lemma less_eq_merge_right: "less_eq y (merge x y)"
+proof -
+  have merge_comm: "merge x y = merge y x" by (rule merge_commutativity)
+  then have "less_eq y (merge y x)" using less_eq_merge_left by simp
+  then show "less_eq y (merge x y)" using merge_comm by simp
+qed
+
+lemma merge_takes_greater: "merge (x#xs) (y#ys) = (if x > y then x#(merge xs ys) else y#(merge xs ys))"
+  apply(induct x)
+  apply (auto)
+  done
+
+lemma less_eq_non_empty:
+  assumes "less_eq (x#xs) c"
+  shows "\<exists> z zs. c = z # zs"
+proof -
+  from assms show ?thesis
+  proof (cases c)
+    case Nil
+    then show ?thesis 
+      using assms
+      by (cases "x # xs", auto)
+  next
+    case (Cons z zs)
+    then show ?thesis by auto
+  qed
+qed
+
+lemma less_eq_merge_bound: "less_eq a c \<Longrightarrow> less_eq b c \<Longrightarrow> less_eq (merge a b) c"
+proof (induct a arbitrary: b c)
+  case Nil
+  then show ?case
+  proof -
+    from merge_empty_left have "merge [] b = b" by simp
+    then have "less_eq (merge [] b) c" using Nil.prems by simp
+    then show ?thesis by assumption
+  qed
+next
+  case (Cons x xs)
+  then show ?case
+  proof (induct b)
+    case Nil
+    then show ?case
+      apply (auto)
+      done
+  next
+    case (Cons y ys)
+    then show ?case
+    proof (induct c)
+      case Nil
+      then show ?case
+        apply (auto)
+        done
+    next
+      case (Cons z zs)
+      then show ?case
+        by simp
+    qed
+  qed
+qed
+
+lemma update_monotonicity: "less_eq a (update a u)"
+proof (induct a arbitrary: u)
+  case Nil
+  then show ?case
+    apply (auto)
+    done
+  case (Cons x xs)
+  then show ?case
+  proof (cases u)
+    case 0
+    then show ?thesis
+      apply (auto)
+      apply (simp add: less_eq_reflexive)
+      done
+  next
+    case (Suc n)
+    then show ?thesis
+      apply (auto) using Cons.hyps less_eq.simps(3) by blast
+  qed
+qed
+
+interpretation IntVector2CvRDT : CvRDT
   IntegerVector.less_eq
   IntegerVector.less
-  IntegerVector.max
+  IntegerVector.merge
   IntegerVector.initial
-  IntegerVector.current
+  IntegerVector.query
   IntegerVector.update
 proof
     show "\<And>x. IntegerVector.less_eq x x"
-      by (simp add: less_eq_self)
+      by (simp add: less_eq_reflexive)
     show "\<And>x y. IntegerVector.less x y = (IntegerVector.less_eq x y \<and> \<not> IntegerVector.less_eq y x)"
       by (simp add: less_comb)
     show "\<And>x y z.
        IntegerVector.less_eq x y \<Longrightarrow>
        IntegerVector.less_eq y z \<Longrightarrow> IntegerVector.less_eq x z"
-      sorry
+      by (simp add: less_eq_transitive)
     show "\<And>x y. IntegerVector.less_eq x y \<Longrightarrow>
            IntegerVector.less_eq y x \<Longrightarrow> x = y"
-      sorry
-    show "\<And>x y. IntegerVector.less_eq x (IntegerVector.max x y)"
-      sorry
-    show "\<And>y x. IntegerVector.less_eq y (IntegerVector.max x y)"
-      sorry
+      by (simp add: less_eq_antisymmetry)
+    show "\<And>x y. IntegerVector.less_eq x (IntegerVector.merge x y)"
+      by (simp add: less_eq_merge_left)
+    show "\<And>y x. IntegerVector.less_eq y (IntegerVector.merge x y)"
+      by (simp add: less_eq_merge_right)
     show "\<And>y x z.
        IntegerVector.less_eq y x \<Longrightarrow>
        IntegerVector.less_eq z x \<Longrightarrow>
-       IntegerVector.less_eq (IntegerVector.max y z) x"
-      sorry
+       IntegerVector.less_eq (IntegerVector.merge y z) x"
+      by (simp add: less_eq_merge_bound)
     show "\<And>a u. IntegerVector.less_eq a (update a u)"
-      sorry
+      by (simp add: update_monotonicity)
 qed
-  
 end
---
+++ ROOT
@@ -0,0 +1,8 @@
+session FV = HOL + 
+    options [document = pdf, document_output = "output"]
+theories
+    CvRDT
+    Log
+    PNSet
+    USet
+    IntegerVector
