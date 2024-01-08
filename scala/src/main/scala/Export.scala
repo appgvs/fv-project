e49b7598ef8823481de8d765abaf3af342a0e97c
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-01-08 14:06:30 +0100

    Update export from isabelle

--- scala/src/main/scala/Export.scala
+++ scala/src/main/scala/Export.scala
@@ -5,10 +5,10 @@ trait equal[A] {
 }
 def equal[A](a: A, b: A)(implicit A: equal[A]): Boolean = A.`HOL.equal`(a, b)
 object equal {
-  implicit def `Log.equal_LogEntry`[A : equal]: equal[Log.LogEntry[A]] = new
-    equal[Log.LogEntry[A]] {
-    val `HOL.equal` = (a: Log.LogEntry[A], b: Log.LogEntry[A]) =>
-      Log.equal_LogEntrya[A](a, b)
+  implicit def `Product_Type.equal_prod`[A : equal, B : equal]: equal[(A, B)] =
+    new equal[(A, B)] {
+    val `HOL.equal` = (a: (A, B), b: (A, B)) =>
+      Product_Type.equal_proda[A, B](a, b)
   }
   implicit def `Nat.equal_nat`: equal[Nat.nat] = new equal[Nat.nat] {
     val `HOL.equal` = (a: Nat.nat, b: Nat.nat) => Nat.equal_nata(a, b)
@@ -19,70 +19,71 @@ def eq[A : equal](a: A, b: A): Boolean = equal[A](a, b)
 
 } /* object HOL */
 
-object Nat {
+object Product_Type {
 
-abstract sealed class nat
-final case class zero_nat() extends nat
-final case class Suc(a: nat) extends nat
-
-def equal_nata(x0: nat, x1: nat): Boolean = (x0, x1) match {
-  case (zero_nat(), Suc(x2)) => false
-  case (Suc(x2), zero_nat()) => false
-  case (Suc(x2), Suc(y2)) => equal_nata(x2, y2)
-  case (zero_nat(), zero_nat()) => true
+def equal_proda[A : HOL.equal, B : HOL.equal](x0: (A, B), x1: (A, B)): Boolean =
+  (x0, x1) match {
+  case ((x1, x2), (y1, y2)) => HOL.eq[A](x1, y1) && HOL.eq[B](x2, y2)
 }
 
-def one_nat: nat = Suc(zero_nat())
-
-def less_eq_nat(x0: nat, n: nat): Boolean = (x0, n) match {
-  case (Suc(m), n) => less_nat(m, n)
-  case (zero_nat(), n) => true
+def snd[A, B](x0: (A, B)): B = x0 match {
+  case (x1, x2) => x2
 }
 
-def less_nat(m: nat, x1: nat): Boolean = (m, x1) match {
-  case (m, Suc(n)) => less_eq_nat(m, n)
-  case (n, zero_nat()) => false
+} /* object Product_Type */
+
+object Orderings {
+
+trait ord[A] {
+  val `Orderings.less_eq`: (A, A) => Boolean
+  val `Orderings.less`: (A, A) => Boolean
+}
+def less_eq[A](a: A, b: A)(implicit A: ord[A]): Boolean =
+  A.`Orderings.less_eq`(a, b)
+def less[A](a: A, b: A)(implicit A: ord[A]): Boolean = A.`Orderings.less`(a, b)
+object ord {
+  implicit def `Nat.ord_nat`: ord[Nat.nat] = new ord[Nat.nat] {
+    val `Orderings.less_eq` = (a: Nat.nat, b: Nat.nat) => Nat.less_eq_nat(a, b)
+    val `Orderings.less` = (a: Nat.nat, b: Nat.nat) => Nat.less_nat(a, b)
+  }
 }
 
-def plus_nat(x0: nat, n: nat): nat = (x0, n) match {
-  case (Suc(m), n) => plus_nat(m, Suc(n))
-  case (zero_nat(), n) => n
+trait preorder[A] extends ord[A] {
+}
+object preorder {
+  implicit def `Nat.preorder_nat`: preorder[Nat.nat] = new preorder[Nat.nat] {
+    val `Orderings.less_eq` = (a: Nat.nat, b: Nat.nat) => Nat.less_eq_nat(a, b)
+    val `Orderings.less` = (a: Nat.nat, b: Nat.nat) => Nat.less_nat(a, b)
+  }
 }
 
-def minus_nat(m: nat, n: nat): nat = (m, n) match {
-  case (Suc(m), Suc(n)) => minus_nat(m, n)
-  case (zero_nat(), n) => zero_nat()
-  case (m, zero_nat()) => m
+trait order[A] extends preorder[A] {
+}
+object order {
+  implicit def `Nat.order_nat`: order[Nat.nat] = new order[Nat.nat] {
+    val `Orderings.less_eq` = (a: Nat.nat, b: Nat.nat) => Nat.less_eq_nat(a, b)
+    val `Orderings.less` = (a: Nat.nat, b: Nat.nat) => Nat.less_nat(a, b)
+  }
 }
 
-} /* object Nat */
+trait linorder[A] extends order[A] {
+}
+object linorder {
+  implicit def `Nat.linorder_nat`: linorder[Nat.nat] = new linorder[Nat.nat] {
+    val `Orderings.less_eq` = (a: Nat.nat, b: Nat.nat) => Nat.less_eq_nat(a, b)
+    val `Orderings.less` = (a: Nat.nat, b: Nat.nat) => Nat.less_nat(a, b)
+  }
+}
+
+def max[A : ord](a: A, b: A): A = (if (less_eq[A](a, b)) b else a)
+
+} /* object Orderings */
 
 object Lista {
-
-def nth[A](x0: List[A], x1: Nat.nat): A = (x0, x1) match {
-  case (x :: xs, Nat.Suc(n)) => nth[A](xs, n)
-  case (x :: xs, Nat.zero_nat()) => x
-}
-
-def drop[A](n: Nat.nat, x1: List[A]): List[A] = (n, x1) match {
-  case (n, Nil) => Nil
-  case (n, x :: xs) => (n match {
-                          case Nat.zero_nat() => x :: xs
-                          case Nat.Suc(m) => drop[A](m, xs)
-                        })
-}
 
 def fold[A, B](f: A => B => B, x1: List[A], s: B): B = (f, x1, s) match {
   case (f, x :: xs, s) => fold[A, B](f, xs, (f(x))(s))
   case (f, Nil, s) => s
-}
-
-def take[A](n: Nat.nat, x1: List[A]): List[A] = (n, x1) match {
-  case (n, Nil) => Nil
-  case (n, x :: xs) => (n match {
-                          case Nat.zero_nat() => Nil
-                          case Nat.Suc(m) => x :: take[A](m, xs)
-                        })
 }
 
 def filter[A](p: A => Boolean, x1: List[A]): List[A] = (p, x1) match {
@@ -98,19 +99,15 @@ def member[A : HOL.equal](x0: List[A], y: A): Boolean = (x0, y) match {
 def insert[A : HOL.equal](x: A, xs: List[A]): List[A] =
   (if (member[A](xs, x)) xs else x :: xs)
 
+def map[A, B](f: A => B, x1: List[A]): List[B] = (f, x1) match {
+  case (f, Nil) => Nil
+  case (f, x21 :: x22) => f(x21) :: map[A, B](f, x22)
+}
+
 def removeAll[A : HOL.equal](x: A, xa1: List[A]): List[A] = (x, xa1) match {
   case (x, Nil) => Nil
   case (x, y :: xs) =>
     (if (HOL.eq[A](x, y)) removeAll[A](x, xs) else y :: removeAll[A](x, xs))
-}
-
-def equal_list[A : HOL.equal](x0: List[A], x1: List[A]): Boolean = (x0, x1)
-  match {
-  case (Nil, x21 :: x22) => false
-  case (x21 :: x22, Nil) => false
-  case (x21 :: x22, y21 :: y22) =>
-    HOL.eq[A](x21, y21) && equal_list[A](x22, y22)
-  case (Nil, Nil) => true
 }
 
 } /* object Lista */
@@ -120,6 +117,10 @@ object Set {
 abstract sealed class set[A]
 final case class seta[A](a: List[A]) extends set[A]
 final case class coset[A](a: List[A]) extends set[A]
+
+def image[A, B](f: A => B, x1: set[A]): set[B] = (f, x1) match {
+  case (f, seta(xs)) => seta[B](Lista.map[A, B](f, xs))
+}
 
 def insert[A : HOL.equal](x: A, xa1: set[A]): set[A] = (x, xa1) match {
   case (x, coset(xs)) => coset[A](Lista.removeAll[A](x, xs))
@@ -154,52 +155,14 @@ def minus_set[A : HOL.equal](a: set[A], x1: set[A]): set[A] = (a, x1) match {
 
 } /* object Set */
 
-object Log {
+object Lattices_Big {
 
-abstract sealed class LogEntry[A]
-final case class LogEntrya[A](a: A, b: List[Nat.nat]) extends LogEntry[A]
-
-def equal_LogEntrya[A : HOL.equal](x0: LogEntry[A], x1: LogEntry[A]): Boolean =
-  (x0, x1) match {
-  case (LogEntrya(x1, x2), LogEntrya(y1, y2)) =>
-    HOL.eq[A](x1, y1) && Lista.equal_list[Nat.nat](x2, y2)
+def Max[A : Orderings.linorder](x0: Set.set[A]): A = x0 match {
+  case Set.seta(x :: xs) =>
+    Lista.fold[A, A](((a: A) => (b: A) => Orderings.max[A](a, b)), xs, x)
 }
 
-abstract sealed class Loga[A]
-final case class Logb[A](a: Set.set[LogEntry[A]]) extends Loga[A]
-
-abstract sealed class NewEvent[A]
-final case class NewEventa[A](a: A, b: Nat.nat, c: List[Nat.nat]) extends
-  NewEvent[A]
-
-def union[A : HOL.equal](x0: Loga[A], x1: Loga[A]): Loga[A] = (x0, x1) match {
-  case (Logb(s1), Logb(s2)) => Logb[A](Set.sup_set[LogEntry[A]](s1, s2))
-}
-
-def vector_clock_update(i: Nat.nat, v: List[Nat.nat]): List[Nat.nat] =
-  {
-    val incremented =
-      Nat.plus_nat(Lista.nth[Nat.nat](v, i), Nat.one_nat): Nat.nat;
-    Lista.take[Nat.nat](i, v) ++
-      (List(incremented) ++
-        Lista.drop[Nat.nat](Nat.plus_nat(i, Nat.one_nat), v))
-  }
-
-def update[A : HOL.equal](x0: Loga[A], x1: NewEvent[A]): Loga[A] = (x0, x1)
-  match {
-  case (Logb(s), NewEventa(e, i, v)) =>
-    Logb[A](Set.sup_set[LogEntry[A]](s, Set.insert[LogEntry[A]](LogEntrya[A](e,
-                                      vector_clock_update(i, v)),
-                         Set.bot_set[LogEntry[A]])))
-}
-
-def initial[A]: Loga[A] = Logb[A](Set.bot_set[LogEntry[A]])
-
-def elements[A](x0: Loga[A]): Set.set[LogEntry[A]] = x0 match {
-  case Logb(s) => s
-}
-
-} /* object Log */
+} /* object Lattices_Big */
 
 object USet {
 
@@ -211,16 +174,79 @@ def add[A : HOL.equal](x0: USeta[A], e: A): USeta[A] = (x0, e) match {
     USetb[A](Set.sup_set[A](s, Set.insert[A](e, Set.bot_set[A])))
 }
 
-def merge[A : HOL.equal](x0: USeta[A], x1: USeta[A]): USeta[A] = (x0, x1) match {
+def merge[A : HOL.equal](x0: USeta[A], x1: USeta[A]): USeta[A] = (x0, x1) match
+  {
   case (USetb(s1), USetb(s2)) => USetb[A](Set.sup_set[A](s1, s2))
 }
 
-def query[A](x0: USeta[A]): Set.set[A] = x0 match { case USetb(s) => s
+def query[A](x0: USeta[A]): Set.set[A] = x0 match {
+  case USetb(s) => s
 }
 
 def initial[A]: USeta[A] = USetb[A](Set.bot_set[A])
 
 } /* object USet */
+
+object Nat {
+
+abstract sealed class nat
+final case class zero_nat() extends nat
+final case class Suc(a: nat) extends nat
+
+def equal_nata(x0: nat, x1: nat): Boolean = (x0, x1) match {
+  case (zero_nat(), Suc(x2)) => false
+  case (Suc(x2), zero_nat()) => false
+  case (Suc(x2), Suc(y2)) => equal_nata(x2, y2)
+  case (zero_nat(), zero_nat()) => true
+}
+
+def less_nat(m: nat, x1: nat): Boolean = (m, x1) match {
+  case (m, Suc(n)) => less_eq_nat(m, n)
+  case (n, zero_nat()) => false
+}
+
+def less_eq_nat(x0: nat, n: nat): Boolean = (x0, n) match {
+  case (Suc(m), n) => less_nat(m, n)
+  case (zero_nat(), n) => true
+}
+
+def one_nat: nat = Suc(zero_nat())
+
+def plus_nat(x0: nat, n: nat): nat = (x0, n) match {
+  case (Suc(m), n) => plus_nat(m, Suc(n))
+  case (zero_nat(), n) => n
+}
+
+def minus_nat(m: nat, n: nat): nat = (m, n) match {
+  case (Suc(m), Suc(n)) => minus_nat(m, n)
+  case (zero_nat(), n) => zero_nat()
+  case (m, zero_nat()) => m
+}
+
+} /* object Nat */
+
+object Log {
+
+def merge[A : HOL.equal](s1: USet.USeta[(A, Nat.nat)],
+                          s2: USet.USeta[(A, Nat.nat)]):
+      USet.USeta[(A, Nat.nat)]
+  =
+  USet.merge[(A, Nat.nat)](s1, s2)
+
+def max_timestamp[A](s: USet.USeta[(A, Nat.nat)]): Nat.nat =
+  Lattices_Big.Max[Nat.nat](Set.image[(A, Nat.nat),
+                                       Nat.nat](((a: (A, Nat.nat)) =>
+          Product_Type.snd[A, Nat.nat](a)),
+         USet.query[(A, Nat.nat)](s)))
+
+def insert[A : HOL.equal](l: USet.USeta[(A, Nat.nat)], e: A):
+      USet.USeta[(A, Nat.nat)]
+  =
+  USet.add[(A, Nat.nat)](l, (e, Nat.plus_nat(max_timestamp[A](l), Nat.one_nat)))
+
+def initial_log[A]: USet.USeta[A] = USet.initial[A]
+
+} /* object Log */
 
 object PNSet {
 
@@ -262,7 +288,8 @@ def initial[A]: PNSeta[A] = PNSetb[A](Set.bot_set[A], Set.bot_set[A])
 
 object IntegerVector {
 
-def merge(v1: List[Nat.nat], x1: List[Nat.nat]): List[Nat.nat] = (v1, x1) match {
+def merge(v1: List[Nat.nat], x1: List[Nat.nat]): List[Nat.nat] = (v1, x1) match
+  {
   case (v1, Nil) => v1
   case (Nil, v :: va) => v :: va
   case (x :: xs, y :: ys) => (if (Nat.less_nat(y, x)) x else y) :: merge(xs, ys)
